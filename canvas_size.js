ead1c61436290ad3aee8d1bdcd8c3e40f401abe2
takenokogohan <51778676+takenokogohan@users.noreply.github.com>
2019-11-13 14:52:50 +0100

    Add files via upload

---
+++ canvas_size.js
@@ -0,0 +1,6 @@
+var container = document.getElementById('canvas-container');
+var canvas = document.getElementById('canvas');
+
+//親要素のサイズをCanvasに指定
+canvas.width = container.clientWidth;
+canvas.height = container.clientHeight;
